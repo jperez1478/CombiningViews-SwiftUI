f1536c7a9a0e7ed574c5ec55de41d7cfba2cc88c
jperez1478 <jessicperez3@gmail.com>
2021-11-19 23:01:49 -0600

    Initial Commit

---
+++ CombingViews/CombingViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombingViewsApp.swift
+//  CombingViews
+//
+//  Created by Jessica Perez on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct CombingViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombingViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombingViews
+//
+//  Created by Jessica Perez on 11/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
